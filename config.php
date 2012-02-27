1fdcb96cbf1db012147d0e4b46d674a09d96759b
nicox <nico@nicox.org>
2012-02-27 21:03:15 +0100

    the first config file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$mysqlhost = "localhost";
+$mysqluser = "username";
+$mysqlpass = "password";
+$dbname = "mac";
+
+$switche5424 = array("");# IP of Dell-Switche Series 5xxx
+$switche6248 = array("");# IP of Dell-Switche Series 6xxx
+$switcheHP28 = array("");# IP of HP Switche Series 28xx
+$switcheHP18 = array("");# IP of HP Switche Series 18xx
+
+
+?>
